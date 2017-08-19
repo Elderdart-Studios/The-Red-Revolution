2371fa2d13cce7c76d746b67d1ae6246402ddd31
Bobby-Z <31147594+Bobby-Z@users.noreply.github.com>
2017-08-18 23:21:23 -0400

    0.02

---
+++ The-Red-Revolution/Assets/Raw/Scripts/MainMenu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour 
+{
+
+	void Start () 
+	{
+
+
+
+	}
+
+	void Update () 
+	{
+
+
+
+	}
+
+	public void SinglePlayerBTNClicked()
+	{
+
+		// SceneManager.LoadScene ("SinglePlayerMenu");
+
+	}
+
+	public void MultiPlayerBTNClicked()
+	{
+
+		// SceneManager.LoadScene ("MultiPlayerMenu");
+
+	}
+
+	public void SkirmishBTNClicked()
+	{
+
+		// SceneManager.LoadScene ("SkirmishMenu");
+
+	}
+
+	public void OptionsBTNClicked()
+	{
+
+		// SceneManager.LoadScene ("OptionsMenu");
+
+	}
+
+	public void ExitBTNClicked()
+	{
+
+		#if UNITY_EDITOR
+
+			UnityEditor.EditorApplication.isPlaying = false;
+
+		#else
+
+			Application.Quit ();
+
+		#endif
+
+	}
+
+}
