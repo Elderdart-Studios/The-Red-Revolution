a12d6ad529fef0b5dcfa85b69054a29d763943f0
Bobby-Z <31147594+Bobby-Z@users.noreply.github.com>
2017-08-18 22:39:39 -0400

    0.01

---
+++ The-Red-Revolution/Assets/MainMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour 
+{
+
+	void Start () 
+	{
+
+
+
+	}
+
+	void Update () 
+	{
+
+
+
+	}
+
+	public void SinglePlayerBTNClicked()
+	{
+
+
+
+	}
+
+	public void MultiPlayerBTNClicked()
+	{
+
+
+
+	}
+
+	public void SkirmishBTNClicked()
+	{
+
+
+
+	}
+
+	public void OptionsBTNClicked()
+	{
+
+
+
+	}
+
+	public void ExitBTNClicked()
+	{
+
+
+
+	}
+
+}
